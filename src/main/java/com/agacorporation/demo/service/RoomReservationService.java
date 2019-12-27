d30d52b72ff262196b51019adbb0a0925759ef86
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-27 21:37:09 +0100

    [add] dependency injection into service

--- src/main/java/com/agacorporation/demo/service/RoomReservationService.java
+++ src/main/java/com/agacorporation/demo/service/RoomReservationService.java
@@ -1,6 +1,24 @@
 package com.agacorporation.demo.service;
 
+import com.agacorporation.demo.domain.Room;
+import com.agacorporation.demo.domain.RoomReservation;
+import com.agacorporation.demo.domain.RoomType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface RoomReservationService {
 
+    List<Room> getAllRooms();
 
+    List<RoomType> getAllRoomTypes();
+
+    Page<RoomReservation> getAllRoomReservations(Pageable pageable);
+
+    RoomReservation getRoomReservations(Long id);
+
+    void deleteRoomReservation(Long id);
+
+    void saveRoomReservation(RoomReservation roomReservation);
 }
--- src/main/java/com/agacorporation/demo/service/RoomReservationServiceImpl.java
+++ src/main/java/com/agacorporation/demo/service/RoomReservationServiceImpl.java
@@ -1,6 +1,21 @@
 package com.agacorporation.demo.service;
 
+import com.agacorporation.demo.repository.RoomRepository;
+import com.agacorporation.demo.repository.RoomReservationRepository;
+import com.agacorporation.demo.repository.RoomTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class RoomReservationServiceImpl {
+
+    @Autowired
+    private RoomTypeRepository roomTypeRepository;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private RoomReservationRepository roomReservationRepository;
+
 
 
 }
