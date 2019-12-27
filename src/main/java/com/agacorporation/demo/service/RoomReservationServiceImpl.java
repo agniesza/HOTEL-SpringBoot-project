0a3d7b999ab533fec2274b0c33c3874ccde86299
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-27 21:38:29 +0100

    [add] implement methods from interface in service

--- src/main/java/com/agacorporation/demo/service/RoomReservationServiceImpl.java
+++ src/main/java/com/agacorporation/demo/service/RoomReservationServiceImpl.java
@@ -1,11 +1,18 @@
 package com.agacorporation.demo.service;
 
+import com.agacorporation.demo.domain.Room;
+import com.agacorporation.demo.domain.RoomReservation;
+import com.agacorporation.demo.domain.RoomType;
 import com.agacorporation.demo.repository.RoomRepository;
 import com.agacorporation.demo.repository.RoomReservationRepository;
 import com.agacorporation.demo.repository.RoomTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public class RoomReservationServiceImpl {
+import java.util.List;
+
+public class RoomReservationServiceImpl implements RoomReservationService {
 
     @Autowired
     private RoomTypeRepository roomTypeRepository;
@@ -17,5 +24,33 @@ public class RoomReservationServiceImpl {
     private RoomReservationRepository roomReservationRepository;
 
 
+    @Override
+    public List<Room> getAllRooms() {
+        return null;
+    }
 
+    @Override
+    public List<RoomType> getAllRoomTypes() {
+        return null;
+    }
+
+    @Override
+    public Page<RoomReservation> getAllRoomReservations(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public RoomReservation getRoomReservations(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteRoomReservation(Long id) {
+
+    }
+
+    @Override
+    public void saveRoomReservation(RoomReservation roomReservation) {
+
+    }
 }
