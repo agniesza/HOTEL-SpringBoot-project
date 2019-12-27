b087b76cfa8fa9a703baf1ef77a37d8607bf002d
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-27 22:04:03 +0100

    [change] implemented methods

--- src/main/java/com/agacorporation/demo/service/RoomReservationServiceImpl.java
+++ src/main/java/com/agacorporation/demo/service/RoomReservationServiceImpl.java
@@ -3,15 +3,19 @@ package com.agacorporation.demo.service;
 import com.agacorporation.demo.domain.Room;
 import com.agacorporation.demo.domain.RoomReservation;
 import com.agacorporation.demo.domain.RoomType;
+import com.agacorporation.demo.exceptions.RoomReservationNotFoundException;
 import com.agacorporation.demo.repository.RoomRepository;
 import com.agacorporation.demo.repository.RoomReservationRepository;
 import com.agacorporation.demo.repository.RoomTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class RoomReservationServiceImpl implements RoomReservationService {
 
     @Autowired
@@ -26,31 +30,41 @@ public class RoomReservationServiceImpl implements RoomReservationService {
 
     @Override
     public List<Room> getAllRooms() {
-        return null;
+        return roomRepository.findAll();
     }
 
     @Override
     public List<RoomType> getAllRoomTypes() {
-        return null;
+        return roomTypeRepository.findAll();
     }
 
     @Override
     public Page<RoomReservation> getAllRoomReservations(Pageable pageable) {
-        return null;
+        Page page;
+        page = roomReservationRepository.findAll(pageable);
+
+        return page;
     }
 
     @Override
     public RoomReservation getRoomReservations(Long id) {
-        return null;
+        Optional<RoomReservation> optionalRoomReservation = roomReservationRepository.findById(id);
+        RoomReservation roomReservation = optionalRoomReservation.orElseThrow(()->new RoomReservationNotFoundException(id));
+        return roomReservation;
     }
 
     @Override
     public void deleteRoomReservation(Long id) {
+        if(roomReservationRepository.existsById(id) == true){
+          roomReservationRepository.deleteById(id);
+        }else{
+            throw new RoomReservationNotFoundException(id);
+        }
 
     }
 
     @Override
     public void saveRoomReservation(RoomReservation roomReservation) {
-
+            roomReservationRepository.save(roomReservation);
     }
 }
