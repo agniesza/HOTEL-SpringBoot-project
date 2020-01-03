0493c2921d4a6d525ffb4f28d12e7ee40d51ef66
agnieszka <agnieszka.gfkm@o2.pl>
2020-01-03 11:18:06 +0100

    [add] showForm method

--- src/main/java/com/agacorporation/demo/component/ReservationFormController.java
+++ src/main/java/com/agacorporation/demo/component/ReservationFormController.java
@@ -3,7 +3,11 @@ package com.agacorporation.demo.component;
 import com.agacorporation.demo.domain.RoomReservation;
 import com.agacorporation.demo.service.RoomReservationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -16,6 +20,17 @@ public class ReservationFormController {
     public ReservationFormController(RoomReservationService roomReservationService)
     {
         this.roomReservationService = roomReservationService;
+    }
+
+    @RequestMapping(value="/reservationForm.html", method= RequestMethod.GET)
+    public String showForm(Model model, Optional<Long> id){
+
+        model.addAttribute("roomReservation",
+                id.isPresent()?
+                        roomReservationService.getRoomReservations(id.get()):
+                        new RoomReservation());
+
+        return "reservationForm";
     }
 
 }
