8ac26cf94109d9f2c111137f54fae8e19494d95f
agnieszka <agnieszka.gfkm@o2.pl>
2020-01-23 14:11:48 +0100

    [change] in repo.init.

--- src/main/java/com/agacorporation/demo/config/RepositoriesInitializer.java
+++ src/main/java/com/agacorporation/demo/config/RepositoriesInitializer.java
@@ -39,17 +39,30 @@ public class RepositoriesInitializer {
                     Authority roleUser = authorityRepository.save(new Authority("ROLE_USER"));
                     Authority roleAdmin = authorityRepository.save(new Authority("ROLE_ADMIN"));
 
-                    User user = new User("user");
-                    user.setAuthorities(new HashSet<>(Arrays.asList(roleUser)));
-                    user.setPassword(passwordEncoder.encode("user"));
-
-                    User admin = new User("admin");
-                    admin.setAuthorities(new HashSet<>(Arrays.asList(roleAdmin)));
-                    admin.setPassword(passwordEncoder.encode("admin"));
-
                     User test = new User("test");
                     test.setAuthorities(new HashSet<>(Arrays.asList(roleUser)));
                     test.setPassword(passwordEncoder.encode("test"));
+                    test.setFirstName("Urszula");
+                    test.setLastName("Kowalska");
+                    test.setPhoneNumber("333333333");
+                    test.setEmail("mail@mial.com");
+                    User user = new User("user");
+                    user.setAuthorities(new HashSet<>(Arrays.asList(roleUser)));
+                    user.setPassword(passwordEncoder.encode("user"));
+                    user.setFirstName("Tomasz");
+                    user.setLastName("Momasz");
+                    user.setEmail("tom@tp.pm");
+                    user.setPhoneNumber("575839328");
+
+                    User admin = new User("admin");
+                    admin.setFirstName("Agnieszka");
+                    admin.setLastName("Agnieszka");
+                    admin.setEmail("aga@aga.aga");
+                    admin.setPhoneNumber("7394638459");
+                    admin.setAuthorities(new HashSet<>(Arrays.asList(roleAdmin)));
+                    admin.setPassword(passwordEncoder.encode("admin"));
+
+
 
 
                     userRepository.save(user);
